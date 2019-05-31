ff9afa425c943c5721ff21b363a5a5c3808befed
Julian Schäfer <julian.david.schaefer@gmail.com>
2019-05-31 16:25:14 +0200

    Added Express

--- application/src/main.ts
+++ application/src/main.ts
@@ -1,57 +1,31 @@
-import { FileSystemWallet, Gateway, GatewayOptions } from "fabric-network";
-import * as fs from "fs";
-import * as yaml from "js-yaml";
+import * as express from 'express';
 
-async function main() {
-    const gateway = new Gateway();
-    const wallet = new FileSystemWallet('/Users/julian/Documents/wallets/identity/user/balaji/wallet');
+let app = express();
 
-    try {
-        const userName = 'Admin@org1.example.com';
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-        // Load connection profile; will be used to locate a gateway
-        let connectionProfile = yaml.safeLoad(fs.readFileSync('networkConnection.yaml', 'utf8'));
+var responses = [];
 
-        // Set connection options; identity and wallet
-        let connectionOptions: GatewayOptions = {
-            identity: userName,
-            wallet: wallet,
-            discovery: { enabled: false, asLocalhost: true }
+app.get('/event-stream', (req, res) => {
+    // SSE Setup
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+    });
+    res.write('\n');
 
-        };
+    responses.push(res);
 
-        // Connect to gateway using application specified parameters
-        console.log('Connect to Fabric gateway.');
+    req.on('close', () => {
+        console.log("Closed");
+    });
+});
 
-        await gateway.connect(connectionProfile, connectionOptions);
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
 
-        // Access PaperNet network
-        console.log('Use network channel: mychannel.');
-
-        const network = await gateway.getNetwork('mychannel');
-
-        // Get addressability to commercial paper contract
-        console.log('Use org.papernet.commercialpaper smart contract.');
-
-        const contract = await network.getContract('Wastechain', 'OrderContract');
-        const tx = await contract.submitTransaction('createOrder', 'test', 'Testvalue');
-
-        contract.addContractListener('CREATE_ORDER', 'CREATE_ORDER', (error: Error, event: { [key: string]: any }, blockNumber: string, transactionId: string, status: string) => {
-            return new Promise((resolve, reject) => {
-                console.log('Order Created: ' + event.payload.toString('utf-8'));
-                if (error) {
-                    reject(error)
-                } else {
-                    resolve(event);
-                }
-            });
-        }, { filtered: false });
-    } catch (e) {
-        console.log(e);
-    } finally {
-        console.log("Disconnecting from Fabric gateway.");
-        gateway.disconnect();
-    }
-}
-
-main().then(() => console.log("Finished"), (e) => console.log("Error: " + e));
+let messageId = 0;
