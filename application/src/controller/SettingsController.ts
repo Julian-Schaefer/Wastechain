d32de60e41599463bd15027bdf1008ed05f9f6cd
Julian Schäfer <julian.david.schaefer@gmail.com>
2019-06-20 21:03:21 +0200

    Settings Endpoints

---
+++ application/src/controller/SettingsController.ts
@@ -0,0 +1,42 @@
+import { Express, Request, Response } from 'express';
+import { Settings, SettingsSchema } from '../model/Settings';
+import { Validator, ValidationError } from 'express-json-validator-middleware';
+import * as fs from 'fs';
+
+export class SettingsController {
+    private FILE_NAME = 'wastechain.json';
+    private validator = new Validator({ allErrors: true });
+
+    constructor(app: Express) {
+        let validator = this.validator.validate({ body: SettingsSchema });
+        app.get('/settings', this.getSettings.bind(this));
+        app.post('/settings', validator, this.postSettings.bind(this));
+    }
+
+    private getSettings(_: Request, response: Response) {
+        fs.readFile(this.FILE_NAME, (error, data: Buffer) => {
+            if (error) {
+                response.send('Error reading Settings.');
+                console.log('Error reading Settings: ' + error);
+            } else {
+                let settings = JSON.parse(data.toString('utf8')) as Settings;
+                response.setHeader('Content-Type', 'application/json');
+                response.send(settings);
+            }
+        });
+    }
+
+    private async postSettings(request: Request, response: Response) {
+        let settings = request.body as Settings;
+
+        fs.writeFile(this.FILE_NAME, JSON.stringify(settings), 'utf8', (error) => {
+            if (error) {
+                response.send('Error writing Settings.');
+                console.log('Error writing Settings: ' + error);
+            } else {
+                response.setHeader('Content-Type', 'application/json');
+                response.send(settings);
+            }
+        });
+    }
+}
--- application/src/server.ts
+++ application/src/server.ts
@@ -1,16 +1,20 @@
 import * as express from 'express';
+import * as bodyparser from 'body-parser';
 import { OrderController } from './controller/OrderController';
 import { FabricConnection } from './fabric';
+import { SettingsController } from './controller/SettingsController';
 
 export class WastechainServer {
     private app = express();
     private _orderController: OrderController;
 
     constructor(fabricConnection: FabricConnection) {
+        this.app.use(bodyparser.json());
         this.app.listen(3000, function () {
             console.log('Wastechain-Server listening on port 3000!');
         });
         this._orderController = new OrderController(this.app, fabricConnection);
+        new SettingsController(this.app);
     }
 
     get getOrderController(): OrderController {
