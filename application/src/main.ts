36a9b9161479a5ee4b22a14ace619902a81c6f46
Julian Schäfer <julian.david.schaefer@gmail.com>
2019-06-25 22:13:19 +0200

    Added dotenv

--- application/src/main.ts
+++ application/src/main.ts
@@ -1,14 +1,17 @@
 import { FabricConnection } from './fabric';
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
+import * as dotenv from 'dotenv';
 import { WastechainServer } from './server';
 
 async function main() {
+    dotenv.config();
 
     const username = 'Admin@org1.example.com';
     const channelName = 'mychannel';
     const walletLocation = '../identity/user/balaji/wallet';
-    let connectionProfile = yaml.safeLoad(fs.readFileSync('networkConnection.yaml', 'utf8'));
+    let networkConnectionString = fs.readFileSync('networkConnection.yaml', 'utf8');
+    let connectionProfile = yaml.safeLoad(networkConnectionString.split('${FABRIC_NETWORK_URL}').join(process.env.FABRIC_NETWORK_URL));
 
     const fabricConnection = new FabricConnection(username, channelName, walletLocation, connectionProfile);
     await fabricConnection.connect();
