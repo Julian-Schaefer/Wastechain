099ecce329f3c7c8fa7601a63e5b84d7eeb2ed9c
Julian Schäfer <julian.david.schaefer@gmail.com>
2019-07-05 16:07:29 +0200

    Added test-network

---
+++ test-network/generate.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+CHANNEL_NAME=wastechain
+
+# remove previous crypto material and config transactions
+rm -fr config/*
+rm -fr crypto-config/*
+
+# generate crypto material
+cryptogen generate --config=./crypto-config.yaml
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate crypto material..."
+  exit 1
+fi
+
+# generate genesis block for orderer
+configtxgen -profile WastechainOrdererGenesis -outputBlock ./config/genesis.block
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate orderer genesis block..."
+  exit 1
+fi
+
+# generate channel configuration transaction
+configtxgen -profile WastechainChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate channel configuration transaction..."
+  exit 1
+fi
+
+# generate anchor peer transaction
+configtxgen -profile WastechainChannel -outputAnchorPeersUpdate ./config/OrderingOrgMSPanchors.tx -channelID $CHANNEL_NAME -asOrg OrderingOrg
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for OrderingOrg..."
+  exit 1
+fi
+
+configtxgen -profile WastechainChannel -outputAnchorPeersUpdate ./config/SubcontractorOrgMSPanchors.tx -channelID $CHANNEL_NAME -asOrg SubcontractorOrg
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for SubcontractorOrg..."
+  exit 1
+fi
+
+configtxgen -profile WastechainChannel -outputAnchorPeersUpdate ./config/ThirdPartyOrgMSPanchors.tx -channelID $CHANNEL_NAME -asOrg ThirdPartyOrg
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for ThirdPartyOrg..."
+  exit 1
+fi
