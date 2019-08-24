08fa8f8011a53f34e6c11e0558e3bc77b288e6a1
Julian Schäfer <julian.david.schaefer@gmail.com>
2019-08-24 18:39:24 +0200

    Corrected Server CORS

--- application/src/server.ts
+++ application/src/server.ts
@@ -7,51 +7,52 @@ import * as SwaggerJSDoc from 'swagger-jsdoc';
 import SettingsRouter from './settings/SettingsRouter';
 import WasteOrderRouter from './wasteOrder/WasteOrderRouter';
 
-const app = express();
-
-app.use(bodyparser.json());
-
-const options: SwaggerJSDoc.Options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Wastechain-API',
-            version: '1.0.0'
-        },
-    },
-    basePath: '/',
-    // Path to the API docs
-    apis: ['**/*.ts'],
-};
-
-// Initialize swagger-jsdoc -> returns validated swagger spec in json format
-const swaggerSpec = SwaggerJSDoc(options);
-
-app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(swaggerSpec));
-
-
-app.use((req: express.Request, _, next) => {
-    console.log('Logger:' + JSON.stringify(req.body));
-    next();
-});
-
-app.use(function (error: Error, _: any, response: any, next: any) {
-    if ((error as ValidationError).isJoi) {
-        response.status(400).send('Invalid Format of the Request Body.');
-        next();
-    }
-    else {
-        next(error); // pass error on if not a validation error
-    }
-});
-
-app.use('/settings', SettingsRouter);
-app.use('/order', WasteOrderRouter);
-
 export function startServer() {
+    const app = express();
+
     if (process.env.ALLOW_CORS === 'true') {
+        console.log('Allowing CORS');
         app.use(cors());
     }
+
+    app.use(bodyparser.json());
+
+    const options: SwaggerJSDoc.Options = {
+        definition: {
+            openapi: '3.0.0',
+            info: {
+                title: 'Wastechain-API',
+                version: '1.0.0'
+            },
+        },
+        basePath: '/',
+        // Path to the API docs
+        apis: ['**/*.ts'],
+    };
+
+    // Initialize swagger-jsdoc -> returns validated swagger spec in json format
+    const swaggerSpec = SwaggerJSDoc(options);
+
+    app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(swaggerSpec));
+
+
+    app.use((req: express.Request, _, next) => {
+        console.log('Logger:' + JSON.stringify(req.body));
+        next();
+    });
+
+    app.use(function (error: Error, _: any, response: any, next: any) {
+        if ((error as ValidationError).isJoi) {
+            response.status(400).send('Invalid Format of the Request Body.');
+            next();
+        }
+        else {
+            next(error); // pass error on if not a validation error
+        }
+    });
+
+    app.use('/settings', SettingsRouter);
+    app.use('/order', WasteOrderRouter);
 
     app.listen(process.env.PORT, function () {
         console.log('Wastechain-Server listening on port ' + process.env.PORT + '!');
