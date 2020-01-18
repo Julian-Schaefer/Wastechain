b6eccb5c2cd78f458af1ed65f34d239bd7edb1e1
Julian Schäfer <julian.david.schaefer@gmail.com>
2020-01-18 13:31:45 +0100

    Not using Prod Dockerfile

--- test-network/start_api.sh
+++ test-network/start_api.sh
@@ -1,4 +1,4 @@
 docker build ../application --tag wastechain.azurecr.io/wastechain-api:master
-docker build ../frontend -f ../frontend/Dockerfile.prod --tag wastechain.azurecr.io/wastechain-frontend:master
+docker build ../frontend --tag wastechain.azurecr.io/wastechain-frontend:master
 
 docker-compose -f api-compose.yml up -d
