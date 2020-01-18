1fa17fa1260f154e000e857122cccf259b46e79e
Julian Schäfer <julian.david.schaefer@gmail.com>
2020-01-18 22:45:47 +0100

    Fixed Connection

--- test-network/start.sh
+++ test-network/start.sh
@@ -23,7 +23,7 @@ export FABRIC_START_TIMEOUT=10
 sleep ${FABRIC_START_TIMEOUT}
 
 # Create the channel
-docker exec cli peer channel create -o orderer.wastechain.org:7050 -c wastechain -f /etc/hyperledger/config/channel.tx
+docker exec -e "CORE_PEER_LOCALMSPID=OrderingOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/crypto-config/peerOrganizations/ordering-organisation.com/users/Admin@ordering-organisation.com/msp" -e "CORE_PEER_ADDRESS=peer0.ordering-organisation.com:7051" cli peer channel create -o orderer.wastechain.org:7050 -c wastechain -f /etc/hyperledger/config/channel.tx
 
 # Join peer0.ordering-organisation.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=OrderingOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/crypto-config/peerOrganizations/ordering-organisation.com/users/Admin@ordering-organisation.com/msp" -e "CORE_PEER_ADDRESS=peer0.ordering-organisation.com:7051" cli peer channel join -b wastechain.block
